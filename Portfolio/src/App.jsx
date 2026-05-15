11c93b5ae2cb38501b4aa8dd57ca6fb5707394a3
EsterHajos <ester.marta.hajos@gmail.com>
2026-05-15 09:41:35 +0200

    added code from my other VS that had an error

---
+++ Portfolio/src/App.jsx
@@ -0,0 +1,13 @@
+import Component from './components/projectcard';
+import './App.css';
+import './index.css';
+
+function App() {
+  return (
+    <div>
+      <Component />
+    </div>
+  );
+}
+
+export default App;
