c5bdf204fb4899e060960f339d043799320f595c
EsterHajos <ester.marta.hajos@gmail.com>
2026-06-02 10:58:21 +0200

    corrected misspellings and did a new install - portfol is visible now

--- Portfolio/src/data/projects.js
+++ Portfolio/src/data/projects.js
@@ -24,6 +24,18 @@ const projects = [
     role: "I was responsible for...",
     challenges: "One of the challenges I faced was..."
   }
+
+   {
+    projectname: "Project 3",
+    screenshot: "",
+    githubRepo: "https://github.com/",
+    techUsed: ["React", "Node.js", "Express"],
+    writeUp: "This is a project about...",
+    projectfunction: "The project allows users to...",
+    learned: "I learned how to...",
+    role: "I was responsible for...",
+    challenges: "One of the challenges I faced was..."
+  }
 ];
 
 export default projects;
