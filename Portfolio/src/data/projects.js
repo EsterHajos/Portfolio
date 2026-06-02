9602c0f64315ae1293d86d680b3398f4acad0104
EsterHajos <ester.marta.hajos@gmail.com>
2026-06-02 11:22:30 +0200

    checked misspellning and added card info

--- Portfolio/src/data/projects.js
+++ Portfolio/src/data/projects.js
@@ -1,35 +1,33 @@
-// Project data for the portfolio
-
 const projects = [
   {
-    projectname: "Project 1",
-    screenshot: "",
-    githubRepo: "https://github.com/",
-    techUsed: ["React", "Node.js", "Express"],
-    writeUp: "This is a project about...",
-    projectfunction: "The project allows users to...",
-    learned: "I learned how to...",
-    role: "I was responsible for...",
-    challenges: "One of the challenges I faced was..."
+    projectname: "Esters 1st HTML - HTML lesson 1",
+    screenshot: "/images/esters1sthtml.png",
+    githubRepo: "https://github.com/EsterHajos/esters1sthtml.git",
+    techUsed: ["HTML"],
+    writeUp: "This was my absolute first assignment when starting Front End Development. Here we just started with learning how to create an index.html file and how to use basic HTML tags. I created a simple webpage about myself with a header, a paragraph, an image and a link.",
+    projectfunction: "No special functions just a basic HTML webpage with basic text tags and no design.",
+    learned: "I learned how to create a basic HTML file and how to use basic HTML tags such as <h1>, <p>, <img> and <a>.",
+    role: "It was a solo project where I was responsible for everything.",
+    challenges: "To learn how to gitbash."
   },
 
   {
-    projectname: "Project 2",
-    screenshot: "",
-    githubRepo: "https://github.com/",
-    techUsed: ["React", "Node.js", "Express"],
-    writeUp: "This is a project about...",
-    projectfunction: "The project allows users to...",
-    learned: "I learned how to...",
-    role: "I was responsible for...",
-    challenges: "One of the challenges I faced was..."
-  }
+    projectname: "Figma lesson 4",
+    screenshot: "/images/figma4.png",
+    githubRepo: "https://github.com/EsterHajos/figma4.git",
+    techUsed: ["HTML", "CSS"],
+    writeUp: "This is a project where we had to recreate a design from Figma using HTML and CSS. I chose to recreate a design of a website for a music festival. The design was quite simple but it was a good exercise to practice using CSS to style a webpage.",
+    projectfunction: "There's no special functions, it's just a static webpage with some styling.",
+    learned: "I learned how to use CSS to recreate a design from Figma and how to use different CSS properties to style a webpage.",
+    role: "I was the sole developer for this project.",
+    challenges: "My main challange was placement of the elements and to get the design to look exactly like the Figma design. I had to use a lot of trial and error to get the placement right and to find the right CSS properties to use."
+  },
 
-   {
-    projectname: "Project 3",
-    screenshot: "",
-    githubRepo: "https://github.com/",
-    techUsed: ["React", "Node.js", "Express"],
+  {
+    projectname: "Form lesson 7",
+    screenshot: "/images/form7.png",
+    githubRepo: "https://github.com/EsterHajos/Form7.git",
+    techUsed: ["HTML", "CSS"],
     writeUp: "This is a project about...",
     projectfunction: "The project allows users to...",
     learned: "I learned how to...",
