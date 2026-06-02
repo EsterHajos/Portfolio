2ee0c095fd7157814df02d0d1394733264595c18
EsterHajos <ester.marta.hajos@gmail.com>
2026-06-02 14:39:46 +0200

    trying to clean up my mess with the folders

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,7 +1,3 @@
-import Projects from "./data/Projects";
-import Projectcard from "./components/Projectcard";
-
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -20,26 +16,5 @@ function App() {
     </BrowserRouter>
   );
 } 
-
-export default App;
-
-
-
-function App() {
-  return (
-    <div>
-      <h1>My Portfolio</h1>
-
-      <div className="projects-container">
-        {Projects.map((project) => (
-          <Projectcard
-            key={project.projectname}
-            {...project}
-          />
-        ))}
-      </div>
-    </div>
-  );
-}
 
 export default App;
