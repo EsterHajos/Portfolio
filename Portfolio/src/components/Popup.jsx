0a41fcf5c808fc3677252f402a9b99c5327be9df
EsterHajos <ester.marta.hajos@gmail.com>
2026-06-03 10:41:56 +0200

    started creating popup

---
+++ Portfolio/src/components/Popup.jsx
@@ -0,0 +1,30 @@
+function Popup ({
+projectname,
+screenshot,
+githubRepo,
+techUsed,
+writeUp,
+projectfunction,
+learned,
+role,
+challanges,
+onClose,
+
+})
+
+
+{    return (
+        <div className="popup">
+            <div className="popup-content">
+
+                <h2>{projectname}</h2>
+                <img src={screenshot} alt={`${projectname} screenshot`} />
+                <p><strong>GitHub Repository:</strong> <a href={githubRepo} target="_blank" rel="noopener noreferrer">{githubRepo}</a></p>
+                <p><strong>Technologies Used:</strong> {techUsed}</p>
+                <p><strong>Project Function:</strong> {projectfunction}</p>
+                <p><strong>What I Learned:</strong> {learned}</p>
+                <p><strong>My Role:</strong> {role}</p>
+                <p><strong>Challenges Faced:</strong> {challanges}</p>
+                <p><strong>Write-Up:</strong> {writeUp}</p>
+                 
+                <button onClick={onClose}>Close</button>
